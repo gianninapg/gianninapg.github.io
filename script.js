75450c9be465ac0dae9031a34028cdb064d9387f
gianninapg@gmail.com <Gi@090488>
2020-07-16 15:46:28 +1000

    js updated.

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ document.getElementById("box").onmouseover = function() {mouseOver()};
 document.getElementById("box").onmouseout = function() {mouseOut()};
 
 function mouseOver() {
-  document.getElementById("box").style.background = "red";
+  document.getElementById("box").style.backgroundColor = "red";
 }
 
 function mouseOut() {
-  document.getElementById("box").style.background = "black";
+  document.getElementById("box").style.backgroundColor = "black";
+}
+function changeColour(box) {
+    // start
+    document.body.style.backgroundColor = box;
+    // end
 }
